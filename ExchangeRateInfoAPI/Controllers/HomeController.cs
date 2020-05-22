2bee0f7d0dc6e409a92a69236edc6d80fba1a1ec
Aadil Hussain Aadil <aaadil@dxc.com>
2020-05-22 18:40:52 +0530

    Local VS Setup

--- ExchangeRateInfoAPI/Controllers/HomeController.cs
+++ ExchangeRateInfoAPI/Controllers/HomeController.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace ExchangeRateInfoAPI.Controllers
-{
+{ 
+    /// <summary>
+    /// Home Controller
+    /// </summary>
     public class HomeController : Controller
     {
         public ActionResult Index()
